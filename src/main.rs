c5955a33590c771a05962e04e71d5f086bedfaf3
lukidom <swingfighter>
2026-01-22 15:25:06 -0700

    1.1 Update. Started adding multy threading. Practiced with a sleep request.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::io::Error;
+use std::thread;
+use std::time::Duration;
 use std::fs;
 fn main() {
     let listener: TcpListener = 
@@ -10,35 +12,34 @@ fn main() {
             handle_connection(stream);
         }
 }
-
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+    println!("Received request: {}", String::from_utf8_lossy(&buffer));
+
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("index.html").unwrap();
+    let (status_line, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-        let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    
+    let contents = 
+        fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nCOntent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = 
-            fs::read_to_string("404.html").unwrap();
-
-        let response = format!(
-            "{}\r\nCOntent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
